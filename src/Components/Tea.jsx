ff90a4ab574c5207202803a9c71d8a35c2a9c912
IsaacSantos1 <isaacsantos0902@gmail.com>
2026-01-21 10:35:40 -0500

    New

--- src/Components/Tea.jsx
+++ src/Components/Tea.jsx
@@ -1,4 +1,4 @@
-import styles from '../Styles/Tea.module.css';  
+import styles from '../styles/Tea.module.css';  
 import p1 from '../assets/Teas/1.jpg';
 import p2 from '../assets/Teas/2.jpg';
 import p3 from '../assets/Teas/3.jpg';
