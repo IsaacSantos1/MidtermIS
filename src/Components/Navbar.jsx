571665982228d644698d21e78a18160b25a5f44b
IsaacSantos1 <isaacsantos0902@gmail.com>
2026-01-22 09:54:43 -0500

    Fixed minor changes and made it mobile responsive with an introduction of a mobile button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,38 @@
-import styles from '../styles/Navbar.module.css'
-import logo from '../assets/logo.png'
-import { Link } from 'react-router-dom'
+import styles from '../styles/Navbar.module.css'; 
+import logo from '../assets/logo.png'; 
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Navbar() {
+    const [isToggled, setIsToggled] = useState(false);
+    const linksClassName = isToggled ? styles.toggledMenu : styles.notToggledMenu;
 
-    return(
+    const handleMenu = () => {
+        setIsToggled(!isToggled);
+    };
+
+    return (
         <nav>
             <Link to='/'><img className={styles.logo} src={logo} alt="TeaCozy" /></Link>
-            <ul className={styles.router}>
-                <Link className={styles.link} to='/Hero'>Our Mission</Link>
-                <Link className={styles.link} to='/Tea'>Featured Tea</Link>
-                <Link className={styles.link} to='/Locations'>Locations</Link>
-            </ul>
+
+            <div className={styles.toggleContainer}>
+                <button className={styles.navButton} onClick={handleMenu}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu-icon lucide-menu">
+                        <path d="M4 5h16" />
+                        <path d="M4 12h16" />
+                        <path d="M4 19h16" />
+                    </svg>
+                </button>
+
+                <ul className={linksClassName}>
+                    <Link className={styles.link} to='/Hero'>Our Mission</Link>
+                    <Link className={styles.link} to='/Tea'>Featured Tea</Link>
+                    <Link className={styles.link} to='/Locations'>Locations</Link>
+                </ul>
+            </div>
         </nav>
     );
 }
 
-export default Navbar
+export default Navbar;
+
